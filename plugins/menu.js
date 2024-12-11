0cd76d21004856667dd26d055025e4b088a7701c
Arslan-MD <157588592+ArslanMDofficial@users.noreply.github.com>
2024-12-12 01:12:50 +0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -4,15 +4,17 @@ cmd({
     pattern: "menu",
     desc: "menu the bot",
     category: "menu",
-    react: "🪄",
+    react: "💋",
     filename: __filename
 },
 
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let dec ` I am ArslanMD-BOT `
 
-await conn.sendMessage(from,{video:{url: `https://files.catbox.moe/h6ru3v.mp4`},mimetype:"video/mp4"},{dec:caption},{quoted:mek});
+let dec = `
+I am ArslanMD-BOT
+`
+await conn.sendMessage(from,{video:{url: `https://files.catbox.moe/h6ru3v.mp4`},mimetype:"video/mp4"},{caption:dec},{quoted:mek});
 
 }catch(e){
 console.log(e)
